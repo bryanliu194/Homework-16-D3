cd018a76dd371eb47be8f4ab108a5b95df9ac7a1
Bryan Liu <bryanliu@bryans-mbp.attlocal.net>
2018-06-19 22:26:57 -0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,108 @@
 // D3 Scatterplot Assignment
 
-// Students:
-// =========
-// Follow your written instructions and create a scatter plot with D3.js.
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("./assets/Data/data.csv", function(err, demodata) {
+  if (err) throw err;
+
+  // Step 1: Parse Data/Cast as numbers
+   // ==============================
+  demodata.forEach(function(data) {
+    data.poverty = +data.poverty;
+    data.obesity = +data.obesity;
+  });
+
+  // Step 2: Create scale functions
+  // ==============================
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(demodata, d => d.poverty) - 1, d3.max(demodata, d => d.poverty) + 1])
+    .range([0, width]);
+
+  var yLinearScale = d3.scaleLinear()
+    .domain([20, d3.max(demodata, d => d.obesity)])
+    .range([height, 0]);
+
+
+  // Step 3: Create axis functions
+  // ==============================
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // Step 4: Append Axes to the chart
+  // ==============================
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  chartGroup.append("g")
+    .call(leftAxis);
+
+   // Step 5: Create Circles
+  // ==============================
+  var circlesGroup = chartGroup.selectAll("circle")
+  .data(demodata)
+  .enter()
+  .append("circle")
+  .attr("cx", d => xLinearScale(d.poverty))
+  .attr("cy", d => yLinearScale(d.obesity))
+  .attr("r", "10")
+  .attr("opacity", ".5");
+
+  // Step 6: Initialize tool tip
+  // ==============================
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([0, 70])
+    .html(function(d) {
+      return (`${d.state}<br>Poverty: ${d.poverty}<br>Obesity: ${d.obesity}`);
+    });
+
+  // Step 7: Create tooltip in the chart
+  // ==============================
+  chartGroup.call(toolTip);
+
+  // Step 8: Create event listeners to display and hide the tooltip
+  // ==============================
+  circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data);
+  })
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+      toolTip.hide(data);
+    });
+
+  // Create axes labels
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Obesity (%)");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("Poverty (%)");
+});
